8be832e063c5222c08957ff87fae48ec897613d9
tturb1ne <expiloarmys@gmail.com>
2023-04-05 17:51:47 +0300

    authentificator changed

---
+++ TradeApp/ViewModel/AuthVerificationVM.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using TradeApp.Views;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Channels;
+using System.Windows;
+using TradeApp.Models;
+
+namespace TradeApp.ViewModel
+{
+    internal class AuthVerificationVM : NotifierVM
+    {
+        private string lgn;
+        private string pwd;
+
+        public string Login 
+        {
+            get => lgn;
+            set { lgn = value;
+
+                OnPropertyChanged(nameof(Login));
+            }
+        }
+
+        public string Password
+        {
+            get => pwd;
+            set
+            {
+                pwd = value;
+
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public void Verify()            //Переделал цикл регистрации, теперь должен работать над окном с администраторами
+        {
+            if (lgn == null || pwd == null)
+            {
+                MessageBox.Show("Please fill out the blank fields." + MessageBoxButton.OK + MessageBoxImage.Stop);
+                return;
+            }
+
+            using(var db = new TradeEntities())        
+            {
+                var check = db.User.FirstOrDefault(u => u.UserLogin == lgn && u.UserPassword == pwd);   //Необходимо добавить проверку на логин/пароль админа
+                if (check != null)
+                {
+                    MessageBox.Show("Authorization complete." + MessageBoxButton.OK);
+
+                    var NewAdWindow = new AdminDataWindow();
+
+                    NewAdWindow.Show();
+
+                    foreach (Window wnd in Application.Current.Windows)
+                    {
+                        if (wnd is AuthWindow)
+                        {
+                            wnd.Close();
+                        }
+                    }
+                }
+                else MessageBox.Show("Incorrect Login Or Password" + MessageBoxButton.OK + MessageBoxImage.Warning);
+            }
+        }
+
+        public void GuestAuth()
+        {
+
+        }
+    }
+}
---
+++ TradeApp/Views/AuthWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TradeApp.Models;
+using TradeApp.ViewModel;
+
+namespace TradeApp.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new AuthVerificationVM();
+        }
+
+        private void ButnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as AuthVerificationVM).Verify();
+        }
+    }
+}
